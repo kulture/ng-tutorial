2486fdd975786bcb8ab5f8ee0ee56530c3061a9f
kulture <andrewdavidphillips@gmail.com>
2017-12-18 19:49:14 -0500

    initial commit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,8 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 1, name: 'Kerry' },
+    { id: 2, name: 'Morgan' },
+    { id: 3, name: 'Beth' },
+    { id: 4, name 'Brooke' }
+];
